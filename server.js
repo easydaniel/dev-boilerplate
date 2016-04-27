4b6fb32957409bb3146d24ac5054b3b40baff6b2
easydaniel <whitetiger50512@gmail.com>
2016-04-27 12:42:22 +0800

    init

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.listen(port);
+console.log('Server listening on 0.0.0.0' + ':' + port);
